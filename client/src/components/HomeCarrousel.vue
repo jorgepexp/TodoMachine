26ac07b42f7c3b2fdf06cb7e186e9edfd2cb2968
giorgio <jorge.pexp@gmail.com>
2022-02-09 23:46:07 +0100

    Creada versión inicial de vista Home

---
+++ client/src/components/HomeCarrousel.vue
@@ -0,0 +1,59 @@
+<template>
+  <section id="home-carrousel">
+    <v-carousel
+      v-model="carrousel"
+      hide-delimiter-background
+      show-arrows-on-hover
+    >
+      <v-carousel-item v-for="img in images" :key="img.src">
+        <v-row align="center" justify="center">
+          <img
+            :src="require('../assets/img/' + img.src)"
+            :alt="img.alt"
+            class="carrousel-img"
+          />
+        </v-row>
+      </v-carousel-item>
+    </v-carousel>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'HomeCarrousel',
+  components: {},
+  data() {
+    return {
+      carrousel: 0,
+      // TODO Que las imágenes se pasen al componente como props
+      images: [
+        {
+          src: 'luffy.jpg',
+          alt: 'Luffy',
+        },
+        {
+          src: 'jinbei.jpg',
+          alt: 'Jinbei',
+        },
+        {
+          src: 'zoro.jpg',
+          alt: 'Zoro',
+        },
+      ],
+    };
+  },
+  methods: {},
+};
+</script>
+
+<style lang="scss">
+#home-carrousel {
+  min-height: 100%;
+
+  .carrousel-img {
+    width: 100%;
+    height: 500px;
+    object-fit: contain;
+  }
+}
+</style>
--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,15 +1,70 @@
 <template>
-  <hello-world />
+  <section id="home-view">
+    <main>
+      <h3>Aumenta tu productividad organizando tus tareas en tableros</h3>
+      <p>
+        Ac massa a quisque cursus urna tristique sem accumsan purus lorem lacus
+        sit a hendrerit leo pellentesque consectetur consectetur portaest lacus
+        orci mi pellentesque varius.
+      </p>
+      <v-btn class="register-btn">
+        Regístrate gratis
+      </v-btn>
+    </main>
+    <HomeCarrousel />
+  </section>
 </template>
 
 <script>
-import HelloWorld from '../components/HelloWorld';
-
+import HomeCarrousel from '@/components/HomeCarrousel.vue';
 export default {
   name: 'HomeView',
-
   components: {
-    HelloWorld,
+    HomeCarrousel,
   },
 };
 </script>
+
+<style lang="scss">
+@import url('http://fonts.cdnfonts.com/css/gordita');
+#home-view {
+  min-height: 100%;
+
+  display: flex;
+  flex-flow: column nowrap;
+  flex: 1;
+
+  background-color: var(--surface3);
+  font-family: gordita, Helvetica Neue, Helvetica, sans-serif;
+  padding: 1rem;
+
+  main {
+    margin-bottom: 3rem;
+
+    h3 {
+      font-family: gordita, Helvetica Neue, Helvetica, sans-serif;
+      font-weight: 400;
+      font-size: 2rem;
+      color: var(--text1);
+      line-height: 2.8rem;
+
+      margin: 2rem 0 0.8rem 0;
+    }
+
+    p {
+      line-height: 1.75rem;
+
+      margin-bottom: 2rem;
+    }
+
+    .register-btn {
+      background-color: hsl(0, 0%, 0%);
+      color: hsl(0, 0%, 100%);
+
+      &:hover {
+        background-color: $--primary-color;
+      }
+    }
+  }
+}
+</style>
